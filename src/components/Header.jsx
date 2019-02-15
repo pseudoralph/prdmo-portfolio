5c389a1a452ea7b1ab63336470f691215603021f
Ralph Perdomo <ralph@prdmo.com>
2019-02-15 09:48:35 -0800

    Enable hot module in webpack conf

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.styles = {
+      block: {
+        width: '100vw',
+        backgroundColor: '#e8e8e8',
+        height: '100px'
+      }
+    };
+  }
   render() {
-    return <div>Header works</div>;
+    return <div style={this.styles.block}>Header works</div>;
   }
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: ['react-hot-loader/patch', resolve(__dirname, 'src') + '/index.jsx'],
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    resolve(__dirname, 'src') + '/index.jsx'
+  ],
   output: {
     filename: 'app.bundle.js',
     path: resolve(__dirname, 'build'),
