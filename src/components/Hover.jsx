f52e1eec5934ab17617f2ad2f9ee9e8d8b5a4135
Ralph Perdomo <ralph@prdmo.com>
2019-02-15 15:04:25 -0800

    add binding instead of fat arrow (linting issue)

--- src/components/Hover.jsx
+++ src/components/Hover.jsx
@@ -1,29 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { STYLES } from './styles';
 
 class Hover extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = { hover: false };
+
+    this.toggleHover = this.toggleHover.bind(this);
   }
 
-  toggleHover = () => {
+  toggleHover() {
     this.setState(state => {
       return { hover: state.hover ? false : true };
     });
-    console.log(this.state.hover);
-    // console.log(this.state);
-  };
+  }
 
   render() {
     return (
       <div
         onMouseEnter={this.toggleHover}
         onMouseLeave={this.toggleHover}
-        style={{ display: 'inline' }}
+        style={
+          this.state.hover
+            ? { ...STYLES.section, ...STYLES.sectionHover }
+            : { ...STYLES.section }
+        }
       >
-        {this.state.hover ? '>' : ''}
         {this.props.children}
       </div>
     );
