4d64e6aa9d9b86e919e7ea06d18b09998fdc6191
Ralph Perdomo <ralph@prdmo.com>
2019-02-22 14:15:13 -0800

    save

--- src/components/Vignette.jsx
+++ src/components/Vignette.jsx
@@ -5,29 +5,43 @@ class Vignette extends React.Component {
     super(props);
     this.state = {
       fastestScroll: 0,
-      fadeBackIn: 0
+      fadeBackIn: 0,
+      rgbStartState: 128
     };
     this.hiddenMessage = React.createRef();
 
     this.style = {
       padding: '1em',
-      background: 'rgb(128, 128, 128',
-      color: 'rgb(128, 128, 128',
-
+      background: 'rgb(128, 128, 128)',
+      color: `rgb(${this.state.rgbStartState}, ${this.state.rgbStartState}, ${
+        this.state.rgbStartState
+      })`,
       height: '100vh'
     };
   }
 
   slowFade() {
-    let time = 255;
-    return function() {
-      setInterval(() => console.log(time--), 250);
+    let start = 255;
+    let stop = 128;
+    // this.state.rgbStartState;
+
+    return () => {
+      let fader = setInterval(() => {
+        if (start > stop) {
+          return start--;
+        } else {
+          // this.setState({ fastestScroll: 0 });
+          clearInterval(fader);
+        }
+      }, 50);
     };
   }
 
   unhide(event) {
     const delatRGB =
-      128 + Math.abs(event.deltaY) < 255 ? 127 + Math.abs(event.deltaY) : 255;
+      127 + Math.abs(event.deltaY / 4) < 255
+        ? 127 + Math.abs(event.deltaY / 4)
+        : 255;
 
     if (delatRGB > this.state.fastestScroll) {
       this.setState({ fastestScroll: delatRGB });
