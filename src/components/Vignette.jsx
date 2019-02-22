e0e926e8661d0e7c2d60a6cc26a35ebc3b5c3603
Ralph Perdomo <ralph@prdmo.com>
2019-02-22 11:20:20 -0800

    add ref to access element on page and capture delta from scroll event

--- src/components/Vignette.jsx
+++ src/components/Vignette.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 
-function Vignette() {
-  return (
-    <div
-      onScroll={() => console.log('scrolling...')}
-      style={{ padding: '1em', background: 'grey', height: '100vh' }}
-    >
-      Hi there
-      <div />
-    </div>
-  );
+class Vignette extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrollCount: 0
+    };
+    this.hiddenMessage = React.createRef();
+
+    this.style = { padding: '1em', background: 'grey', height: '100vh' };
+  }
+
+  unhide(event) {
+    // const deltaSpeed = setInterval(()=> )
+    const delatRGB = event.deltaY;
+
+    this.hiddenMessage.current.style.color = `rgb(${delatRGB},${delatRGB},${delatRGB})`;
+  }
+
+  render() {
+    return (
+      <div onWheel={event => this.unhide(event)} style={this.style}>
+        <div ref={this.hiddenMessage}>Hi there</div>
+        <div />
+      </div>
+    );
+  }
 }
 export default Vignette;
